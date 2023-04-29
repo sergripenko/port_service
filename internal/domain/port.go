aa08b43bc7477fcfcfb06d6885197704c606ce17
serg <s.ripenko@infocoin.online>
2023-04-29 11:39:02 +0200

    add domain struct

---
+++ internal/domain/port.go
@@ -0,0 +1,15 @@
+package domain
+
+type Port struct {
+	ID          string
+	Name        string
+	City        string
+	Country     string
+	Alias       []string
+	Regions     []string
+	Coordinates []float64
+	Province    string
+	Timezone    string
+	Unlocs      []string
+	Code        string
+}
